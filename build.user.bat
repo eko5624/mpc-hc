33c2b8aff0731735dcf84860add64f7c1b2273c3
eko5624 <562433403@qq.com>
2022-02-12 11:48:13 +0800

    Create build.user.bat

---
+++ build.user.bat
@@ -0,0 +1,15 @@
+```bat
+@ECHO OFF
+REM [Required for LAVFilters] MSYS2/MinGW paths:
+SET "MPCHC_MSYS=C:\MSYS64"
+SET "MPCHC_MINGW32=C:\MSYS64\mingw64"
+SET "MPCHC_MINGW64=C:\MSYS64\mingw64"
+SET "MSYSTEM=MINGW32"
+SET "MSYS2_PATH_TYPE=inherit"
+REM [Optional] Specify GIT location if it is not already set in %PATH%
+SET "MPCHC_GIT=C:\Program Files\Git"
+REM [Optional] If you plan to modify the translations, install Python 3.8 and set the variable to its path
+SET "MPCHC_PYTHON=C:\Program Files\Python38"
+REM [Optional] If you want to customize the Windows SDK version used, set this variable
+SET "MPCHC_WINSDK_VER=8.1"
+```
