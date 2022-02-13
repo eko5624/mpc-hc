6eb028e9ed158dc1351d54619e89a5438852aa2b
eko5624 <562433403@qq.com>
2022-02-13 09:02:58 +0800

    Update build.user.bat

--- build.user.bat
+++ build.user.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 REM [Required for LAVFilters] MSYS2/MinGW paths:
 SET "MPCHC_MSYS=C:\MSYS64"
-SET "MPCHC_MINGW32=C:\MSYS64\mingw64"
-SET "MPCHC_MINGW64=C:\MSYS64\mingw64"
+SET "MPCHC_MINGW32=C:\msys64\mingw64"
+SET "MPCHC_MINGW64=C:\msys64\mingw64"
 SET "MSYSTEM=MINGW32"
 SET "MSYS2_PATH_TYPE=inherit"
 REM [Optional] Specify GIT location if it is not already set in %PATH%
